4f967fd85c7b2fbfca47c802ebfaa33090633ec8
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-02-20 12:33:37 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,17 @@
 # Add seed data in this file
+puts "Seeding Data, Plsease wait..."
+spells = ["acid-arrow", "animal-messenger", "calm-emotions", "charm-person"]
+
+spells.each do |index|
+    response = RestClient.get "https://www.dnd5eapi.co/api/spells/#{index}"
+
+    spell_hash = JSON.parse(response)
+
+    Spell.create(
+        name: spell_hash["name"],
+        level: spell_hash["level"],
+        description: spell_hash["desc"][0]
+    )
+end
+
+puts "Done!"
